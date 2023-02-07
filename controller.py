7354a5fece624a1a66656239a3317c2cf8ccf612
Darryl Quinn <dman776@gmail.com>
2023-02-06 20:48:05 -0600

    renderer fixes

--- controller.py
+++ controller.py
@@ -81,7 +81,6 @@ def render_thread(metars):
     # last_visualizer = 0
 
     while True:
-        safe_logging.safe_log(".")
         try:
             delta_time = toc - tic
             tic = time.perf_counter()
