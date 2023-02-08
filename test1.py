71e2a22e758ffad4620cd7d0df4495ff43334e20
Darryl Quinn <dman776@gmail.com>
2023-02-08 15:21:59 -0600

    add animation tests

---
+++ test1.py
@@ -0,0 +1,13 @@
+import neopixel
+import board
+from adafruit_led_animation.animation.blink import Blink
+import adafruit_led_animation.color as color
+
+pixel_pin = board.D18
+num_pixels = 50
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels)
+blink = Blink(pixels, 0.5, color.PURPLE)
+
+while True:
+    blink.animate()
+
