a27295b15a65abc477f7a268dd45a7c47cedacf4
Darryl Quinn <dman776@gmail.com>
2023-02-08 15:27:59 -0600

    add animation tests

--- test1.py
+++ test1.py
@@ -1,13 +1,61 @@
 import neopixel
 import board
 from adafruit_led_animation.animation.blink import Blink
-import adafruit_led_animation.color as color
+16from adafruit_led_animation.animation.sparklepulse import SparklePulse
+17from adafruit_led_animation.animation.comet import Comet
+18from adafruit_led_animation.animation.chase import Chase
+19from adafruit_led_animation.animation.pulse import Pulse
+20from adafruit_led_animation.animation.sparkle import Sparkle
+21from adafruit_led_animation.animation.rainbowchase import RainbowChase
+22from adafruit_led_animation.animation.rainbowsparkle import RainbowSparkle
+23from adafruit_led_animation.animation.rainbowcomet import RainbowComet
+24from adafruit_led_animation.animation.solid import Solid
+25from adafruit_led_animation.animation.colorcycle import ColorCycle
+26from adafruit_led_animation.animation.rainbow import Rainbow
+27from adafruit_led_animation.animation.customcolorchase import CustomColorChase
+28from adafruit_led_animation.sequence import AnimationSequence
+29from adafruit_led_animation.color import PURPLE, WHITE, AMBER, JADE, MAGENTA, ORANGE
 
 pixel_pin = board.D18
 num_pixels = 50
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, pixel_order=neopixel.GRB)
-blink = Blink(pixels, 0.5, color.PURPLE)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, pixel_order=neopixel.GRB, auto_write=False)
+
+blink = Blink(pixels, speed=0.5, color=JADE)
+colorcycle = ColorCycle(pixels, speed=0.4, colors=[MAGENTA, ORANGE])
+comet = Comet(pixels, speed=0.01, color=PURPLE, tail_length=10, bounce=True)
+chase = Chase(pixels, speed=0.1, size=3, spacing=6, color=WHITE)
+pulse = Pulse(pixels, speed=0.1, period=3, color=AMBER)
+sparkle = Sparkle(pixels, speed=0.1, color=PURPLE, num_sparkles=10)
+solid = Solid(pixels, color=JADE)
+rainbow = Rainbow(pixels, speed=0.1, period=2)
+sparkle_pulse = SparklePulse(pixels, speed=0.1, period=3, color=JADE)
+rainbow_comet = RainbowComet(pixels, speed=0.1, tail_length=7, bounce=True)
+rainbow_chase = RainbowChase(pixels, speed=0.1, size=3, spacing=2, step=8)
+rainbow_sparkle = RainbowSparkle(pixels, speed=0.1, num_sparkles=15)
+custom_color_chase = CustomColorChase(
+    pixels, speed=0.1, size=2, spacing=3, colors=[ORANGE, WHITE, JADE]
+)
+
+animations = AnimationSequence(
+    comet,
+    blink,
+    rainbow_sparkle,
+    chase,
+    pulse,
+    sparkle,
+    rainbow,
+    solid,
+    rainbow_comet,
+    sparkle_pulse,
+    rainbow_chase,
+    custom_color_chase,
+    advance_interval=5,
+    auto_clear=True,
+)
 
 while True:
-    blink.animate()
+    animations.animate()
+
+
+
 
