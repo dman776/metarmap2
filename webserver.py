5e4dd1f0816a662c364ee113a579a7c519ee9581
Darryl Quinn <dman776@gmail.com>
2023-02-13 20:26:23 -0600

    webserver locate api

--- webserver.py
+++ webserver.py
@@ -83,5 +83,5 @@ class WebServer(object):
 
     def _locate(self, pixnum):
         pix = PixelSubset(self._pixels, int(pixnum), int(pixnum)+1)
-        self._renderer.animate_once(Pulse(pix, speed=0.1, period=0.5, color=WHITE))
+        self._renderer.animate_once(Pulse(pix, speed=0.1, period=0.5, color=WHITE), False)
         return bottle.template('index.tpl', metars=self._metarsObj)
