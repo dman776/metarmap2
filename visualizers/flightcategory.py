545c01331fe47997fc88e65fe6491716a6dd88ac
Darryl Quinn <dman776@gmail.com>
2023-02-18 23:30:53 -0600

    add description to visualizers and web index

--- visualizers/flightcategory.py
+++ visualizers/flightcategory.py
@@ -44,8 +44,7 @@ class FlightCategory(Visualizer):
     def description(self):
         return """
             Display the flight category (ie. VFR, MVFR, IFR, LIFR)
-            where VFR=GREEN, MVFR=BLUE, IFR=RED, LIFR=PURPLE.<br />
-            Lighting will be indicated by flashing YELLOW.
+            where VFR=GREEN, MVFR=BLUE, IFR=RED, LIFR=PURPLE.  Lighting will be indicated by flashing YELLOW.
         """
 
     def update_data(self, data):
