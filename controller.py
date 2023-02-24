6f5c4b13b915cb9fad3cc82a4f2a8ea1536bbea9
Darryl Quinn <dman776@gmail.com>
2023-02-24 14:53:03 -0600

    update config page

--- controller.py
+++ controller.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
     renderer.visualizer = config.data.visualizer.active
 
     # Init DISPLAY
-    disp = None
+    disp: Display
     if config.data.display_screen.enabled:
         disp = Display(airports, metars, renderer)
         disp.message("MAP", display.ICON_INFO,
@@ -198,7 +198,7 @@ if __name__ == '__main__':
     schedule.every().day.at('00:00').do(sched_load_suntimes)    # load sun times and dim the map appropriately
 
     # Start up Web Server to handle UI
-    web_server = webserver.WebServer("0.0.0.0", 80, metars, renderer, disp)
+    web_server = webserver.WebServer("0.0.0.0", 80, metars, renderer, disp or None)
     web_server.run()
 
     # ============== MAIN LOOP =====================
