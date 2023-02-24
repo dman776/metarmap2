b942d2fb4b064036d9c395de39f57a1d6361868a
Darryl Quinn <dman776@gmail.com>
2023-02-24 15:08:06 -0600

    update restart function

--- lib/utils.py
+++ lib/utils.py
@@ -96,4 +96,6 @@ def celsius_to_fahrenheit(temperature_celsius: float):
 
 
 def restart():
+    safe_logging.safe_log(os.path.abspath(__file__))
+
     os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
