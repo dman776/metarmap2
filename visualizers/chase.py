780363ed2883d0d00f95e5e4a22a685c60937e9b
Darryl Quinn <dman776@gmail.com>
2023-03-13 17:47:13 -0500

    add Chase visualizer for testing
    Signed-off-by: Darryl Quinn <dman776@gmail.com>

--- visualizers/chase.py
+++ visualizers/chase.py
@@ -49,4 +49,4 @@ class Chase(Visualizer):
     def update_data(self, data):
         # super().update_data(data)
         # self.__effect__.append(Comet(self.__pix__, speed=0.25, color=[0, 0, 255], tail_length=2, bounce=False))
-        self.__effect__.append(Chase(self.__pix__, speed=0.5, size=1, spacing=50, color=[0, 0, 255]))
+        self.__effect__.append(Chase(self.__pix__, speed=0.5, size=1, spacing=49, color=[0, 0, 255]))
