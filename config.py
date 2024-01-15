fff59e097ff530eabca9796d586976f6b29ef5f4
Darryl Quinn <dman776@gmail.com>
2024-01-14 18:27:25 -0600

    bump version
    Signed-off-by: Darryl Quinn <dman776@gmail.com>

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ except ModuleNotFoundError:
 except NotImplementedError:
     pass
 
-__version__ = "1.9.0"
+__version__ = "1.10.0"
 
 
 class Config(object):
--- controller.py
+++ controller.py
@@ -55,7 +55,7 @@ except ValueError:
 
 # ---------------------------------------------------------------------------
 # Globals
-__version__ = "1.9.0"
+__version__ = "1.10.0"
 CONFIG_FILE = "config.json"
 config = None
 
